64fff40ae144ba618f9b1d38876bc569dc6da5ef
Artem <artem.osmak@ukr.net>
2020-09-19 18:43:37 +0300

    P.21 - Массивы и псевдомассивы

--- script.js
+++ script.js
@@ -1,45 +1,40 @@
 'use strict';
 
-const Part = 'P.20 - Объекты, деструктуризация объектов';
+const Part = 'P.21 - Массивы и псевдомассивы';
 
-const options = {
-    name: 'test',
-    width: 1024,
-    height: 1024,
-    colors: {
-        border: 'black',
-        bg: 'red'
-    },
-    makeTest: function() {
-        console.log('Test');
-    }
-};
+const arr = [2, 13, 26, 8, 10];
+arr.sort(compareNum);
+console.log(arr);
 
-// options.makeTest();
+function compareNum(a, b) {
+    return a - b;
+}
 
-const {border, bg} = options.colors;
-console.log(border);
+// arr[99] = 0;
+// console.log(arr.length);
+// console.log(arr);
+
+// arr.forEach(function(item, i, arr) {
+//     console.log(`${i}: ${item} внутри массива ${arr}`);
+// });
 
 
-// console.log(Object.keys(options).length);
+// arr.pop();
+// arr.push(10);
 
-// console.log(options['colors']['border']);
+// console.log(arr);
 
-// delete options.name;
-
-// console.log(options);
-
-
-// let counter = 0;
-// for (let key in options) {
-//     if (typeof(options[key]) === 'object'){
-//         for (let i in options[key]) {
-//         console.log(`Свойство ${i} имеет значение ${options[key][i]}`);
-//         counter++;
-//         }
-//     } else {
-//         console.log(`Свойство ${key} имеет значение ${options[key]}`);
-//         counter++;
-//     }
+// for (let i = 0; i < arr.length; i++) {
+//     console.log(arr[i]);
 // }
-// console.log(counter);
+
+// for (let value of arr) {
+//     console.log(value);
+// }
+
+// const str = prompt('', '');
+// const products = str.split(', ');
+// products.sort();
+// // console.log(products);
+// console.log(products.join('; '));
+
