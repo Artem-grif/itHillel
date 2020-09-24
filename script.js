0ff18e589d981019e0c21b2e9c4ba15b897dd8f2
Artem <artem.osmak@ukr.net>
2020-09-24 22:40:00 +0300

    27. Задачи с собеседований на понимание основ

--- script.js
+++ script.js
@@ -1,56 +1,49 @@
 'use strict';
 
-const Part = '26. Динамическая типизация в JS';
-
-// To String
-
-// 1)
-console.log(typeof(String(null)));
-console.log(typeof(String(4)));
-
-// 2)
-console.log(typeof(5 + ''));
-
-const num = 5;
-
-console.log('https://vk.com/catalog/' + num);
-
-const fontSize = 26 + 'px';
-
-// To Number
-
-// 1)
-console.log(typeof(Number('4')));
-
-// 2)
-console.log(typeof(+'5'));
-
-// 3)
-console.log(typeof(parseInt('15px', 10)));
+const Part = '27. Задачи с собеседований на понимание основ';
 
 
-let answ = +prompt('Hello', '');
+// let x = 5; alert( x++ ); // (ответ 5)
+// let x = 5; alert( ++x ); // (ответ 6)
 
-// To boolean
 
-// 0, '', null, undefined, NaN;
+// [ ] + false - null + true
+// console.log(typeof('' + false)); // 'false'
+// console.log([] + false - null + true); // 'NaN'
 
-// 1)
 
-let switcher = null;
+// let y = 1;
+// let x = y = 2;
+// console.log(x); // (ответ 2)
 
-if (switcher) {
-    console.log('Working...');
-}
 
-switcher = 1;
+// console.log([] + 1 + 2); // '12'
 
-if (switcher) {
-    console.log('Working...');
-}
 
-// 2)
-console.log(typeof(Boolean('4')));
+// console.log('1'[0]); // 1
 
-// 3)
-console.log(typeof(!!'4'));
+
+// console.log(2 && 1 && null && 0 && undefined);
+// И запинается на лжи
+// Или запинается на правде
+
+
+// console.log(!!( 1 && 2 ) === ( 1 && 2 )); //false
+              // (boolean)        (number)
+
+// console.log( null || 2 && 3 || 4); // 3
+
+
+// const a = [1, 2, 3];
+// const b = [1, 2, 3];
+
+// console.log(a == b); //false
+
+
+// console.log(+'Infinity'); //Infinity(number)
+
+
+// console.log('Ёжик' > 'яблоко'); //false
+
+
+console.log(0 || '' || 2 || undefined || true || false); //2
