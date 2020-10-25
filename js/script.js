709ea821de4c7d9f3dc7d19852e29921fe6bcae3
Artem <artem.osmak@ukr.net>
2020-10-25 14:40:51 +0200

    P30. Практика. Задание на отработку действий со страницей

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const Part = '29. Действия с элементами на странице';
+const Part = '30. Практика. Задание на отработку действий со страницей';
 
 
 const box = document.getElementById('box'),
@@ -14,21 +14,24 @@ const box = document.getElementById('box'),
 // box.style.backgroundColor = 'blue';
 // box.style.width = '500px';
 
+
 box.style.cssText = `background-color: blue; width: 500px;`;
 
 btns[1].style.borderRadius = '100%';
 circles[0].style.backgroundColor = 'red';
 
+
 // for (let i = 0; i < hearts.length; i++) {
 //     hearts[i].style.backgroundColor = 'green';
 // }
+
 
 hearts.forEach(item => {
     item.style.backgroundColor = 'green';
 });
 
 const div = document.createElement('div');
-// const text = document.createTextNode('Тут был я');\
+// const text = document.createTextNode('Тут был я');
 
 div.classList.add('black');
 
